278a6d32be78a6d5da3b47fcf11ae95c8a98cf36
Samson Akanbi <samson.akanbi@outlook.com>
2017-04-26 22:21:56 +0000

    Encrypted token with the Digest::SHA1.hexdigest

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
   
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end  
 end
-
-#Start at 6, on the Odin Project
