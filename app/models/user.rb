9b0f34ea65e3846e3c6c109c2e71c371e2dd324b
Samson Akanbi <samson.akanbi@outlook.com>
2017-04-29 21:17:51 +0000

    Added remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -13,9 +16,20 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
   
-  # Remembers a user in the database for use in persistent sessions.
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
+
+  
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    # update_attribute(:reset_digest,   User.digest(reset_token))
+    # update_attribute(:reset_sent_at,  Time.zone.now)
+    update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+  
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token   = User.new_token
+    self.activation_digest  = User.digest(activation_token)
   end  
+  
 end
